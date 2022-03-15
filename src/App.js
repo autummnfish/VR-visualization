5cffde8e10578688318ae457cd1692befb30bb8f
sanma-mod <mrstrandjpn@gmail.com>
2022-03-15 23:54:56 +0900

    drawing graph

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
+import { useRef, useLayoutEffect } from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import graph from "./graph.json";
+
+function Line({ start, end }) {
+	const ref = useRef();
+	useLayoutEffect(() => {
+		ref.current.geometry.setFromPoints(
+			[start, end].map((point) => new THREE.Vector3(...point)),
+		);
+	}, [start, end]);
+	return (
+		<line ref={ref}>
+			<bufferGeometry />
+			<lineBasicMaterial color="hotpink" />
+		</line>
+	);
+}
 
 function App() {
+	const { links } = graph;
+	const radius = 3;
+	const points = convertPolar2Cartesian(radius);
 	return (
-		<div>
+		<div style={{ width: window.innerWidth, height: window.innerHeight }}>
 			<Canvas>
-				<ambientLight intensity={0.1} />
+				<ambientLight color="red" intensity={0.1} />
 				<directionalLight color="red" position={[0, 0, 5]} />
-				<mesh>
-					<boxGeometry />
-					<meshStandardMaterial />
-				</mesh>
+				<OrbitControls
+					enablePan={true}
+					enableZoom={true}
+					enableRotate={true}
+				/>
+				{points.map((point) => {
+					return (
+						<mesh position={[point.x, point.y, point.z]}>
+							<sphereGeometry args={[0.04]} />
+							<meshStandardMaterial />
+						</mesh>
+					);
+				})}
+				{links.map((link) => {
+					const source = [
+						points[link.source].x,
+						points[link.source].y,
+						points[link.source].z,
+					];
+					const target = [
+						points[link.target].x,
+						points[link.target].y,
+						points[link.target].z,
+					];
+					return <Line start={source} end={target} />;
+				})}
 			</Canvas>
 		</div>
 	);
 }
+
+const convertPolar2Cartesian = (radius) => {
+	const { nodes } = graph;
+	const cartesian = [];
+	nodes.forEach((node) => {
+		cartesian.push({
+			x: radius * Math.sin(node.y) * Math.cos(node.x),
+			y: radius * Math.sin(node.x) * Math.cos(node.y),
+			z: radius * Math.cos(node.y),
+		});
+	});
+	return cartesian;
+};
 
 export default App;
